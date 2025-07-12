24d78e5f8959fa6433e483a0080626092f3db1df
Alcoreza <202310642@fit.edu.ph>
2025-07-12 22:20:31 +0800

    refactor(navbar): adjusted the colors

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add("visible");
-                observer.unobserve(entry.target);
-            }
-        });
-    }, { threshold: 0.1 });
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+          observer.unobserve(entry.target);
+        }
+      });
+    },
+    { threshold: 0.1 }
+  );
 
-    const sections = document.querySelectorAll(".fade-in-section");
-    sections.forEach(section => observer.observe(section));
+  const sections = document.querySelectorAll(".fade-in-section");
+  sections.forEach((section) => observer.observe(section));
 });
