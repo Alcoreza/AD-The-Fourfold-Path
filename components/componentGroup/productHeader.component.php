af10ea913473b966fdc11d2ec8b04c3aa1976b33
Clive-04 <clivebenito21@gmail.com>
2025-07-13 15:27:04 +0800

    feat(component): added productHeader as component

---
+++ components/componentGroup/productHeader.component.php
@@ -0,0 +1,6 @@
+<header class="banner">
+        <img src="/assets/img/banner.jpg" alt="Element Products Banner">
+        <div class="banner-text">
+            <h1>All Products</h1>
+        </div>
+</header>
