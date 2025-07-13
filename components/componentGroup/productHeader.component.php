ae8e43dbbb8609b6cf59ffb6514a2db55a13cf5e
Clive-04 <clivebenito21@gmail.com>
2025-07-13 20:08:57 +0800

    fix(component): changed image name for banner

--- components/componentGroup/productHeader.component.php
+++ components/componentGroup/productHeader.component.php
@@ -1,5 +1,5 @@
 <header class="banner">
-        <img src="/assets/img/banner.jpg" alt="Element Products Banner">
+    <img src="/assets/img/productBanner.jpg" alt="Element Products Banner">
         <div class="banner-text">
             <h1>All Products</h1>
         </div>
