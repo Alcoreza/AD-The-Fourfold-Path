a4ce1432c58ad06c3fcdc5da5e0aaf9df35ffa19
Ozbert <202310393@fit.edu.ph>
2025-07-14 14:17:28 +0800

    fix(database): correct case of 'users' in foreign key reference

--- database/carts.model.sql
+++ database/carts.model.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS carts(
     cart_id SERIAL PRIMARY KEY,
     user_id INT UNIQUE,
-    FOREIGN KEY (user_id) REFERENCES Users (id)
+    FOREIGN KEY (user_id) REFERENCES users (id)
 );
