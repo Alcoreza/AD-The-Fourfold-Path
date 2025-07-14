5e4252690018833776f0c900641ebc2eec4f4104
Ozbert <202310393@fit.edu.ph>
2025-07-14 23:15:38 +0800

    feat(user.sataticData): add initial user static data configuration

---
+++ staticDatas/user.staticData.php
@@ -0,0 +1,8 @@
+return [
+    [
+        'username'   => 'admin',
+        'password'   => 'admin1123', 
+        'email'      => 'admin0503@gmail.com',
+        'role'       => 'admin',
+        'created_at' => '2025-07-01 10:00:00',
+    ];
