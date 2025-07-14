3f6ef2b972a4bbe364c0dcdcf281262ef1d6fa27
Clive-04 <clivebenito21@gmail.com>
2025-07-14 14:36:21 +0800

    feat(component): added cart link in navbar

--- components/componentGroup/navbar.component.php
+++ components/componentGroup/navbar.component.php
@@ -3,6 +3,7 @@
     <ul class="nav-links">
         <li><a href="/index.php">Home</a></li>
         <li><a href="/pages/productPage/index.php">Products</a></li>
+        <li><a href="/pages/cartPage/index.php">Cart</a></li>
         <li><a href="/pages/aboutPage/index.php">About</a></li>
         <li><a href="/pages/loginPage/index.php">Login</a></li>
     </ul>
