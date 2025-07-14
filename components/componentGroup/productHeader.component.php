976e29c1e663deddadd1e807338abc44b34f4fce
Clive-04 <clivebenito21@gmail.com>
2025-07-14 15:59:21 +0800

    feat(component): added tagline

--- components/componentGroup/productHeader.component.php
+++ components/componentGroup/productHeader.component.php
@@ -2,6 +2,7 @@
     <img src="/assets/img/productBanner.jpg" alt="Element Products Banner">
     <div class="banner-text">
         <h1>All Products</h1>
+            <p class="subtagline">Elemental training gear crafted for every bender’s journey.</p>
         <!-- [QA] make this section bigger then add sub texts like a tagline representing the products -->
     </div>
 </header>
