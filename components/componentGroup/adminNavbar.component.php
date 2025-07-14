d37ca7fc6e969f06d347e10742b93c95bf414de2
Clive-04 <clivebenito21@gmail.com>
2025-07-14 22:56:07 +0800

    feat(component): added special navbar for admin

---
+++ components/componentGroup/adminNavbar.component.php
@@ -0,0 +1,7 @@
+<nav class="navbar">
+    <div class="nav-brand">🔧 Avatar Admin</div>
+    <ul class="nav-links">
+        <li><a href="/admin/adminPage.php">Manage Products</a></li>
+    <!-- <li><a href="/admin/logout.php">Logout</a></li> -->
+    </ul>
+</nav>
