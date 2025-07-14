977e7d02febf19f217e48a8671498b69e1225fe0
Clive-04 <clivebenito21@gmail.com>
2025-07-14 23:03:25 +0800

    feat(component): remove manage products and added logout button

--- components/componentGroup/adminNavbar.component.php
+++ components/componentGroup/adminNavbar.component.php
@@ -1,7 +1,6 @@
 <nav class="navbar">
     <div class="nav-brand">🔧 Avatar Admin</div>
     <ul class="nav-links">
-        <li><a href="/admin/adminPage.php">Manage Products</a></li>
-    <!-- <li><a href="/admin/logout.php">Logout</a></li> -->
+        <li><a href="/admin/logout.php">Logout</a></li>
     </ul>
 </nav>
