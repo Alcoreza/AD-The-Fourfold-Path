e9e762f07c3fcdb6626d9fff7a6ca35bc9c79db4
Clive-04 <clivebenito21@gmail.com>
2025-07-14 16:31:07 +0800

    feat(bootsrap): fixed directory of COMPONENTS_PATH

--- bootstrap.php
+++ bootstrap.php
@@ -3,7 +3,7 @@ define('BASE_PATH', realpath(__DIR__));
 define('UTILS_PATH', BASE_PATH . '/utils/');
 define('VENDOR_PATH', BASE_PATH . '/vendor/');
 define('HANDLERS_PATH', BASE_PATH . '/handlers/');
-define('COMPONENTS_PATH', BASE_PATH . '/components/');
+define('COMPONENTS_PATH', BASE_PATH . '/components/componentGroup');
 define('TEMPLATES_PATH', BASE_PATH . '/components/templates');
 define('ASSETS_PATH', BASE_PATH . '/assets/');
 define('DATABASE_PATH', BASE_PATH . '/database');
